c1bd6c8e7a4268bcd85d1258dc28373bbcb3ad89
Marc-Andre Servant <marc@maservant.net>
2024-01-18 11:47:52 -0500

    Update for API changes and add subdomain support. - Add support for requesting certificates for subdomains - Use params argument of requests.get() to properly URL-encode parameters - This fixes issues such as AWS domain validation records with characters not allowed in URLs. - Interpret responses as XML, not "LXML" which is meant for parsing HTML. - Add mandatory "EmailType" parameter to setHosts request and preserve MX record priority.

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!/usr/bin/env python3
 
 import requests
 from bs4 import BeautifulSoup as bs
@@ -15,11 +15,15 @@ client_ip = "0.0.0.0"
 nc_username = "username"
 
 def main(): 
-	domain = os.getenv('CERTBOT_DOMAIN')
+	complete_domain = os.getenv('CERTBOT_DOMAIN')
+	parts = complete_domain.split('.') # [SUB, DOMAIN, SLD, TLD]
+	domain = parts[-2] + '.' + parts[-1] # SLD.TLD
+	subdomain = complete_domain.removesuffix(domain) # SUB.DOMAIN.
+	subdomain = subdomain.removesuffix('.') # SUB.DOMAIN
 	records = get_host_records(domain)
-	records = clean_old_challenges(records)
-	records = append_challenge_tag(records)
-	set_host_records(domain,records)
+	records = clean_old_challenges(records, subdomain)
+	records = append_challenge_tag(records, subdomain)
+	set_host_records(domain, records)
 	time.sleep(60)
 
 def method_url(cmd_name, *args, **kwargs):
@@ -33,67 +37,82 @@ def method_url(cmd_name, *args, **kwargs):
 		api_url = "https://api.sandbox.namecheap.com/xml.response"
 	else:
 		api_url = "https://api.namecheap.com/xml.response"
-	return f"{api_url}?ApiUser={api_username}&ApiKey={api_key}&UserName={nc_username}&ClientIp={client_ip}&Command={cmd_name}"
+	
+	params = { 'ApiUser': api_username, 'ApiKey': api_key, 'UserName': nc_username, 'ClientIp': client_ip, 'Command': cmd_name }
+	return api_url, params
 
 def get_host_records(domain):
 	"""
 	Return list of <Host> elements.
 	Does not yet support mutlipart TLDs.
 	"""
-	url = method_url("namecheap.domains.dns.getHosts", sandbox=SANDBOX)
+	url, args = method_url("namecheap.domains.dns.getHosts", sandbox=SANDBOX)
 	[SLD, TLD] = domain.split('.')
-	url = url + f"&SLD={SLD}&TLD={TLD}"
-	result = requests.get(url).text
-	soup = bs(result, 'lxml')
+	args['SLD'] = SLD
+	args['TLD'] = TLD
+	result = requests.get(url, params=args).text
+	soup = bs(result, 'xml')
 	hosts = soup.find_all('host')
 	return hosts
 
-def clean_old_challenges(records):
+def clean_old_challenges(records, subdomain):
 	"""
 	Removes all old _acme-challenge TXT tags.
 	Returns list of <Host> elements,
 	but without any challenge tags.
 	"""
+	record_name = "_acme-challenge"
+	if (subdomain):
+		record_name = record_name + '.' + subdomain
+
 	for record in records.copy():
-		if record['name'] == "_acme-challenge":
+		if record['Name'] == record_name:
 			records.remove(record)
 	return records
 
-def append_challenge_tag(records):
+def append_challenge_tag(records, subdomain):
 	"""
 	Generate a new challenge tag and append to records.
 	Returns list of <Host> elements,
 	with a new challenge tag appended.
 	"""
+	record_name = "_acme-challenge"
+	if (subdomain):
+		record_name = record_name + '.' + subdomain
+	
 	challenge = os.getenv('CERTBOT_VALIDATION')
-	challenge_tag = bs(f'<host name="_acme-challenge" type="TXT" address="{challenge}" ttl="60"', 'lxml').body.next
+	challenge_soup = bs(f'<Host Name="{record_name}" Type="TXT" Address="{challenge}" TTL="60"', 'xml')
+	challenge_tag = challenge_soup.Host
 	records.append(challenge_tag)
 	return records
 
-def set_host_records(domain,records):
+def set_host_records(domain, records):
 	"""
 	Updates records for domain.
 	Currently only cares about name, type, address, and optional ttl.
 	"""
-	url = method_url("namecheap.domains.dns.setHosts", sandbox=SANDBOX)
+	url, args = method_url("namecheap.domains.dns.setHosts", sandbox=SANDBOX)
 	[SLD, TLD] = domain.split('.')
-	url = url + f"&SLD={SLD}&TLD={TLD}"
+	args['SLD'] = SLD
+	args['TLD'] = TLD
+	args['EmailType'] = 'MX' # For manual MX setup. Lookup documentation if your config is different.
+
 	n = 1
 	for record in records:
-		name = record['name']
-		type = record['type']
-		address = record['address']
-		if record['ttl']:
-			ttl = record['ttl']
-		url = url + f"&HostName{n}={name}&RecordType{n}={type}&Address{n}={address}"
-		if ttl:
-			url = url + f"&TTL{n}={ttl}"
+		args[f'HostName{n}'] = record['Name']
+		args[f'RecordType{n}'] = record['Type']
+		args[f'Address{n}'] = record['Address']
+		if 'TTL' in record.attrs:
+			args[f'TTL{n}'] = record['TTL']
+		if 'MXPref' in record.attrs:
+			args[f'MXPref{n}'] = record['MXPref']
 		n += 1
-	result = requests.get(url).text
-	soup = bs(result, 'lxml')
-	success = soup.find_all(attrs={"issuccess":"true"})
+	
+	result = requests.get(url, params=args).text
+	soup = bs(result, 'xml')
+	success = soup.DomainDNSSetHostsResult["IsSuccess"] == "true"
 	if success:
-		print("Records successfully updated.")
+		print("Namecheap API hook: records successfully updated.")
 
 if __name__ == "__main__":
 	main()
